9756f278cc1a89b544002b4049b816e2c7cee61b
dem3trio <dgzaballos@gmail.com>
2016-07-26 19:09:34 +0200

    optimized imports

--- src/AppBundle/Controller/TemplateController.php
+++ src/AppBundle/Controller/TemplateController.php
@@ -2,10 +2,8 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 
 class TemplateController extends Controller
 {
