a7b9063295ca207c65b3c2e85dd634205dc152d3
dem3trio <dgzaballos@gmail.com>
2016-07-26 19:15:18 +0200

    optimized imports

--- src/AppBundle/Controller/SessionController.php
+++ src/AppBundle/Controller/SessionController.php
@@ -4,7 +4,6 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 
 class SessionController extends Controller
 {
